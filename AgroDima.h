ab03613c13a6f315cfcc778db81a60c38649bebe
Вадим Залюбовский <kingofsystem@ya.ru>
2015-07-17 04:18:29 +0700

    deadline

--- AgroDima.h
+++ AgroDima.h
@@ -7,16 +7,22 @@
 #ifndef _AgroDima_H_
 #define _AgroDima_H_
 #include "Arduino.h"
-#define DEBUG
 #define BUFFER_SIZE 256
+#define DEBUG
 //add your includes for the project AgroDima here
 #include <Ethernet.h>
 #include <SPI.h>
-#include "SdFat.h"
-#include <iBoardRF24.h>
-#include "AgroDB.h"
-#include <digitalWriteFast.h>
+#include <SdFat.h>
+//#include <iBoardRF24.h> // FIXME
+//#include <digitalWriteFast.h> // FIXME
 //end of add your includes here
+// Аёдишники реле и нодов
+#define RELE1 1
+#define RELE2 2
+#define RELE3 3
+
+#define NODE 4
+
 #ifdef __cplusplus
 extern "C" {
 #endif
